a2ac3ceeff38e286fbe631d7fcb70756eaa23fc5
Roman Hodulák <hodulakr@gmail.com>
2024-07-18 15:32:28 +0200

    feat(storage): Add `apply_with_tables`

--- src/storage.rs
+++ src/storage.rs
@@ -1,6 +1,7 @@
 use {
     move_binary_format::errors::PartialVMError,
     move_core_types::{effects::ChangeSet, resolver::MoveResolver},
+    move_table_extension::TableChangeSet,
     move_vm_test_utils::InMemoryStorage,
     std::fmt::Debug,
 };
@@ -17,6 +18,18 @@ pub trait Storage: MoveResolver<Self::Err> {
 
     /// Applies the `changes` to the underlying storage state.
     fn apply(&mut self, changes: ChangeSet) -> Result<(), Self::Err>;
+
+    /// Applies the `changes` to the underlying storage state. In addition, applies `table_changes`
+    /// using the [`move_table_extension`].
+    ///
+    /// Requires `table-extension` feature. The method exists even with this feature being turned
+    /// off to allow using it whether the feature flag is on or not. When off, it behaves like
+    /// [`Storage::apply`]
+    fn apply_with_tables(
+        &mut self,
+        changes: ChangeSet,
+        #[cfg(feature = "table-extension")] table_changes: TableChangeSet,
+    ) -> Result<(), Self::Err>;
 }
 
 impl Storage for InMemoryStorage {
@@ -24,5 +37,13 @@ impl Storage for InMemoryStorage {
 
     fn apply(&mut self, changes: ChangeSet) -> Result<(), PartialVMError> {
         InMemoryStorage::apply(self, changes)
+    }
+
+    fn apply_with_tables(
+        &mut self,
+        changes: ChangeSet,
+        #[cfg(feature = "table-extension")] table_changes: TableChangeSet,
+    ) -> Result<(), PartialVMError> {
+        InMemoryStorage::apply_extended(self, changes, table_changes)
     }
 }
