49365ab0fa75addfb4c6eb370f94055a60e86781
Bulent Tastan <bulenttastan@gmail.com>
2024-08-01 09:57:49 -0400

    Address comments: build inside target folder and delete before start

--- genesis-builder/src/main.rs
+++ genesis-builder/src/main.rs
@@ -1,19 +1,21 @@
 use {
     aptos_framework::{BuildOptions, BuiltPackage, ReleaseBundle, ReleasePackage},
-    move_package::LintFlag,
-    move_package::{package_hooks::register_package_hooks, BuildConfig as MoveBuildConfig},
+    move_package::{
+        package_hooks::register_package_hooks, BuildConfig as MoveBuildConfig, LintFlag,
+    },
     once_cell::sync::Lazy,
-    std::env::temp_dir,
-    std::fs,
-    std::io::{BufRead, BufReader},
-    std::path::{Path, PathBuf},
-    std::process::Command,
+    std::{
+        collections::BTreeMap,
+        fs::{read_to_string, remove_dir_all, write, OpenOptions},
+        io::{BufRead, BufReader},
+        path::PathBuf,
+        process::Command,
+    },
     sui_framework::SystemPackage,
     sui_move_build::{BuildConfig, SuiPackageHooks},
     sui_types::base_types::ObjectID,
 };
 
-pub const CRATE_ROOT: &str = env!("CARGO_MANIFEST_DIR");
 pub const APTOS_SNAPSHOT_NAME: &str = "aptos.mrb";
 pub const SUI_SNAPSHOT_NAME: &str = "sui.mrb";
 pub const MOVE_STDLIB_FOLDER_NAME: &str = "move-stdlib";
@@ -21,38 +23,51 @@ pub const SUI_FRAMEWORK_FOLDER_NAME: &str = "sui-framework";
 pub const SUI_STDLIB_PACKAGE_ID: ObjectID = small_object_id(0x21);
 pub const SUI_FRAMEWORK_PACKAGE_ID: ObjectID = small_object_id(0x22);
 
-const ORIGINAL_SUI_STDLIB_PACKAGE_ID: ObjectID = small_object_id(0x1);
-const ORIGINAL_SUI_FRAMEWORK_PACKAGE_ID: ObjectID = small_object_id(0x2);
 const APTOS_REPO: &str = "https://github.com/aptos-labs/aptos-core";
 const APTOS_REPO_TAG: &str = "aptos-node-v1.14.0";
+const ORIGINAL_SUI_STDLIB_PACKAGE_ID: ObjectID = small_object_id(0x1);
+const ORIGINAL_SUI_FRAMEWORK_PACKAGE_ID: ObjectID = small_object_id(0x2);
 const SUI_REPO: &str = "https://github.com/MystenLabs/sui";
 const SUI_REPO_TAG: &str = "testnet-v1.28.3";
 const MOVE_TOML: &str = "Move.toml";
 
-static APTOS_DIR: Lazy<PathBuf> = Lazy::new(|| {
-    temp_dir()
-        .join("aptos-core")
-        .join("aptos-move")
-        .join("framework")
+static TARGET_ROOT: Lazy<PathBuf> = Lazy::new(|| {
+    PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .expect("Workspace root directory should exist")
+        .join("target")
 });
-static SUI_DIR: Lazy<PathBuf> = Lazy::new(|| {
-    temp_dir()
-        .join("sui")
+static APTOS_DIR: Lazy<PathBuf> = Lazy::new(|| TARGET_ROOT.join("aptos-core"));
+static APTOS_FRAMEWORK_DIR: Lazy<PathBuf> =
+    Lazy::new(|| APTOS_DIR.join("aptos-move").join("framework"));
+static APTOS_PACKAGE_PATHS: Lazy<[PathBuf; 5]> = Lazy::new(|| {
+    [
+        APTOS_FRAMEWORK_DIR.join("move-stdlib"),
+        APTOS_FRAMEWORK_DIR.join("aptos-stdlib"),
+        APTOS_FRAMEWORK_DIR.join("aptos-framework"),
+        APTOS_FRAMEWORK_DIR.join("aptos-token"),
+        APTOS_FRAMEWORK_DIR.join("aptos-token-objects"),
+    ]
+});
+static APTOS_ADDRESS_MAPPING: Lazy<BTreeMap<&str, &str>> = Lazy::new(|| {
+    BTreeMap::from([
+        ("\"0x0\"", "\"0x10\""),
+        // TODO: Map std, aptos_std and aptos_framework address from 0x1 to 0x11
+        ("\"0x3\"", "\"0x13\""),
+        ("\"0x4\"", "\"0x14\""),
+        ("\"0xA\"", "\"0x1A\""),
+        ("\"0xA550C18\"", "\"0x15\""),
+    ])
+});
+static SUI_DIR: Lazy<PathBuf> = Lazy::new(|| TARGET_ROOT.join("sui"));
+static SUI_FRAMEWORK_DIR: Lazy<PathBuf> = Lazy::new(|| {
+    SUI_DIR
         .join("crates")
         .join("sui-framework")
         .join("packages")
 });
-static APTOS_PACKAGE_PATHS: Lazy<[PathBuf; 5]> = Lazy::new(|| {
-    [
-        APTOS_DIR.join("move-stdlib"),
-        APTOS_DIR.join("aptos-stdlib"),
-        APTOS_DIR.join("aptos-framework"),
-        APTOS_DIR.join("aptos-token"),
-        APTOS_DIR.join("aptos-token-objects"),
-    ]
-});
 static SUI_COIN_PATH: Lazy<PathBuf> = Lazy::new(|| {
-    SUI_DIR
+    SUI_FRAMEWORK_DIR
         .join(SUI_FRAMEWORK_FOLDER_NAME)
         .join("sources")
         .join("coin.move")
@@ -68,6 +83,7 @@ fn main() -> anyhow::Result<()> {
 
     clone_repos()?;
 
+    fix_aptos_packages()?;
     build_aptos_packages()?;
 
     fix_sui_packages()?;
@@ -76,36 +92,46 @@ fn main() -> anyhow::Result<()> {
 }
 
 fn clone_repos() -> anyhow::Result<()> {
+    // Always start with fresh copies of framework repos
+    if APTOS_DIR.try_exists()? {
+        remove_dir_all(APTOS_DIR.as_path())?;
+    }
+    if SUI_DIR.try_exists()? {
+        remove_dir_all(SUI_DIR.as_path())?;
+    }
+
     // Clone the Aptos and Sui repos which contain the framework packages
     Command::new("git")
-        .current_dir(&temp_dir())
+        .current_dir(TARGET_ROOT.as_path())
         .args(["clone", "--depth", "1", "-b", APTOS_REPO_TAG, APTOS_REPO])
-        .output()
-        .unwrap();
+        .output()?;
     Command::new("git")
-        .current_dir(&temp_dir())
+        .current_dir(TARGET_ROOT.as_path())
         .args(["clone", "--depth", "1", "-b", SUI_REPO_TAG, SUI_REPO])
-        .output()
-        .unwrap();
+        .output()?;
+    Ok(())
+}
+
+fn fix_aptos_packages() -> anyhow::Result<()> {
+    // Addresses are mapped from 0x1, 0x3, 0x4 to 0x11, 0x13, 0x14 etc for conflict resolution
+    for path in APTOS_PACKAGE_PATHS.iter() {
+        let move_toml_file = &path.join(MOVE_TOML);
+        let mut content = read_to_string(move_toml_file)?;
+        for (from, to) in APTOS_ADDRESS_MAPPING.iter() {
+            content = content.replace(from, to);
+        }
+        write(move_toml_file, content)?;
+    }
     Ok(())
 }
 
 fn build_aptos_packages() -> anyhow::Result<()> {
-    let options = BuildOptions {
-        with_srcs: true,
-        with_abis: false,
-        with_source_maps: false,
-        with_error_map: true,
-        skip_fetch_latest_git_deps: false,
-        bytecode_version: None,
-        ..BuildOptions::default()
-    };
     // Build the framework packages
     let packages = APTOS_PACKAGE_PATHS
         .iter()
         .map(|path| {
             ReleasePackage::new(
-                BuiltPackage::build(path.to_owned(), options.clone())
+                BuiltPackage::build(path.to_owned(), BuildOptions::default())
                     .expect("Aptos package failed to build"),
             )
             .expect("Release package generation failed")
@@ -120,29 +146,34 @@ fn build_aptos_packages() -> anyhow::Result<()> {
             .collect(),
     );
     let binary = bcs::to_bytes(&bundle)?;
-    fs::write(Path::new(CRATE_ROOT).join(APTOS_SNAPSHOT_NAME), binary)?;
+    write(TARGET_ROOT.join(APTOS_SNAPSHOT_NAME), binary)?;
+    println!("Generated Aptos snapshot bundle");
     Ok(())
 }
 
 fn fix_sui_packages() -> anyhow::Result<()> {
     // Addresses are mapped from 0x1, 0x2 to 0x21, 0x22 for conflict resolution
-    let move_toml_file = &SUI_DIR.join(MOVE_STDLIB_FOLDER_NAME).join(MOVE_TOML);
-    let content = fs::read_to_string(move_toml_file)?;
+    let move_toml_file = &SUI_FRAMEWORK_DIR
+        .join(MOVE_STDLIB_FOLDER_NAME)
+        .join(MOVE_TOML);
+    let content = read_to_string(move_toml_file)?;
     let content = content.replace(
         &ORIGINAL_SUI_STDLIB_PACKAGE_ID.to_hex_literal(),
         &SUI_STDLIB_PACKAGE_ID.to_hex_literal(),
     );
-    fs::write(move_toml_file, content)?;
-    let move_toml_file = &SUI_DIR.join(SUI_FRAMEWORK_FOLDER_NAME).join(MOVE_TOML);
-    let content = fs::read_to_string(move_toml_file)?;
+    write(move_toml_file, content)?;
+    let move_toml_file = &SUI_FRAMEWORK_DIR
+        .join(SUI_FRAMEWORK_FOLDER_NAME)
+        .join(MOVE_TOML);
+    let content = read_to_string(move_toml_file)?;
     let content = content.replace(
         &ORIGINAL_SUI_FRAMEWORK_PACKAGE_ID.to_hex_literal(),
         &SUI_FRAMEWORK_PACKAGE_ID.to_hex_literal(),
     );
-    fs::write(move_toml_file, content)?;
+    write(move_toml_file, content)?;
 
     // Remove #[deprecated(..)] annotations as they cause Move compilation issues
-    let file = fs::OpenOptions::new()
+    let file = OpenOptions::new()
         .read(true)
         .write(true)
         .open(SUI_COIN_PATH.as_path())
@@ -153,7 +184,7 @@ fn fix_sui_packages() -> anyhow::Result<()> {
         .filter(|line| !line.contains("#[deprecated"))
         .collect::<Vec<String>>()
         .join("\n");
-    fs::write(SUI_COIN_PATH.as_path(), lines).expect("Can't write to `coin` module");
+    write(SUI_COIN_PATH.as_path(), lines).expect("Can't write to `coin` module");
     Ok(())
 }
 
@@ -179,8 +210,8 @@ fn build_packages_with_move_config(config: MoveBuildConfig) -> anyhow::Result<()
         print_diags_to_stderr: false,
     };
 
-    let move_stdlib_path = &SUI_DIR.join(MOVE_STDLIB_FOLDER_NAME);
-    let sui_framework_path = &SUI_DIR.join(SUI_FRAMEWORK_FOLDER_NAME);
+    let move_stdlib_path = &SUI_FRAMEWORK_DIR.join(MOVE_STDLIB_FOLDER_NAME);
+    let sui_framework_path = &SUI_FRAMEWORK_DIR.join(SUI_FRAMEWORK_FOLDER_NAME);
     let stdlib_cmp_pkg = build_config.clone().build(move_stdlib_path)?;
     let framework_cmp_pkg = build_config.build(sui_framework_path)?;
 
@@ -199,7 +230,7 @@ fn build_packages_with_move_config(config: MoveBuildConfig) -> anyhow::Result<()
 
     // Serialize packages and write to a single file
     let binary = bcs::to_bytes(&packages)?;
-    fs::write(Path::new(CRATE_ROOT).join(SUI_SNAPSHOT_NAME), binary)?;
-    println!("Generated Sui framework packages");
+    write(TARGET_ROOT.join(SUI_SNAPSHOT_NAME), binary)?;
+    println!("Generated Sui snapshot bundle");
     Ok(())
 }
