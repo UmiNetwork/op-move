1b68c3acd77960f6f839b63d855318e1c0e921c4
Roman Hodulák <roman.hodulak@polyglot-software.com>
2025-03-03 15:07:03 +0100

    build: Mention storage backend name in the feature flag
    Also add `storage` flag pointing to `storage-lmdb` as default.

--- server/src/dependency/mod.rs
+++ server/src/dependency/mod.rs
@@ -1,14 +1,14 @@
 #[cfg(feature = "storage-lmdb")]
 pub use heed::*;
-#[cfg(all(not(feature = "storage"), not(feature = "storage-lmdb")))]
+#[cfg(all(not(feature = "storage-rocksdb"), not(feature = "storage-lmdb")))]
 pub use in_memory::*;
-#[cfg(all(feature = "storage", not(feature = "storage-lmdb")))]
+#[cfg(all(feature = "storage-rocksdb", not(feature = "storage-lmdb")))]
 pub use rocksdb::*;
 
 #[cfg(feature = "storage-lmdb")]
 mod heed;
-#[cfg(all(not(feature = "storage"), not(feature = "storage-lmdb")))]
+#[cfg(all(not(feature = "storage-rocksdb"), not(feature = "storage-lmdb")))]
 mod in_memory;
-#[cfg(all(feature = "storage", not(feature = "storage-lmdb")))]
+#[cfg(all(feature = "storage-rocksdb", not(feature = "storage-lmdb")))]
 mod rocksdb;
 mod shared;
