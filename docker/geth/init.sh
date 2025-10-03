d0563907f2969764a29b8c7a88d4707508b7fe59
code-dogg <realcodedogg@protonmail.com>
2025-10-03 17:57:44 +0200

    build(docker): Deploy L1 contracts and generate L2 genesis during `geth` image build

--- docker/geth/init.sh
+++ docker/geth/init.sh
@@ -4,8 +4,17 @@
 # -e Exit if a command fails
 # -u Treat unset or undefined variables as errors
 # -x Print out command arguments during execution
-set -eux
+# -a Export all variables
+set -euxa
 . /volume/.env
+WORKDIR="/volume/packages/contracts-bedrock"
+DEPLOY_CONFIG="${WORKDIR}/deploy-config/umi.json"
+DEPLOY_CONFIG_PATH="${DEPLOY_CONFIG}"
+L1_DEPLOYMENT="${WORKDIR}/deployments/1337-deploy.json"
+L1_RPC_URL="http://0.0.0.0:58138"
+L2_ALLOCS="${WORKDIR}/state-dump-42069.json"
+GENESIS_FILE="${WORKDIR}/deployments/genesis.json"
+ROLLUP_FILE="${WORKDIR}/deployments/rollup.json"
 
 # Wait for the RPC node to become available
 wait-for-it "${L1_RPC_ADDR}:${L1_RPC_PORT}"
@@ -18,3 +27,37 @@ cast publish --rpc-url "${L1_RPC_URL}" "${SIGNED_L1_CONTRACT_TX}"
 
 # Wait for a finalized block with a positive timestamp
 while [ "$(cast block finalized --rpc-url "${L1_RPC_URL}" | awk '/^timestamp/ { print $2 }')" -le 0 ]; do sleep 3; done
+
+# Generate L2 genesis deploy config file
+DEPLOY_CONFIG_PATH="${DEPLOY_CONFIG_PATH}" \
+./config.sh
+
+# Deploy Optimism L1 contracts
+DEPLOYMENT_CONTEXT=umi \
+DEPLOY_CONFIG_PATH="${DEPLOY_CONFIG_PATH}" \
+IMPL_SALT=0000000000000000000000000000000000000000000000000000000000000000 \
+forge script ${WORKDIR}/scripts/Deploy.s.sol:Deploy \
+    --root "${WORKDIR}" \
+    --private-key "${ADMIN_PRIVATE_KEY}" \
+    --broadcast \
+    --rpc-url "${L1_RPC_URL}" \
+    --slow \
+    --legacy \
+    --non-interactive
+
+# Generate L2 genesis state dump
+CONTRACT_ADDRESSES_PATH="${L1_DEPLOYMENT}" \
+DEPLOY_CONFIG_PATH="${DEPLOY_CONFIG_PATH}" \
+forge script ${WORKDIR}/scripts/L2Genesis.s.sol:L2Genesis \
+    --root "${WORKDIR}" \
+    --sig "runWithAllUpgrades()" \
+    --non-interactive
+
+# Generate L2 genesis
+op-node genesis l2 \
+    --deploy-config "${DEPLOY_CONFIG}" \
+    --l1-deployments "${L1_DEPLOYMENT}" \
+    --l2-allocs "${L2_ALLOCS}" \
+    --outfile.l2 "${GENESIS_FILE}" \
+    --outfile.rollup "${ROLLUP_FILE}" \
+    --l1-rpc "${L1_RPC_URL}"
--- docker/op-node/entrypoint.sh
+++ docker/op-node/entrypoint.sh
@@ -6,7 +6,6 @@ set -euxo allexport
 . /volume/.env
 WORKDIR="/volume/packages/contracts-bedrock"
 SHARED="/volume/shared"
-DEPLOY_CONFIG="${WORKDIR}/deploy-config/umi.json"
 ROLLUP_FILE="${WORKDIR}/deployments/rollup.json"
 JWT_FILE="${WORKDIR}/deployments/jwt.txt"
 GENESIS_FILE="${WORKDIR}/deployments/genesis.json"
@@ -15,80 +14,13 @@ L1_RPC_URL="http://geth:58138"
 L2_RPC_URL="http://op-move:8551"
 OP_MOVE_ADDR="op-move"
 OP_MOVE_PORT="8545"
-L2_ALLOCS="${WORKDIR}/state-dump-42069.json"
 
-# Remove genesis config to prevent other services using outdated genesis
-if [ ! -f "${ROLLUP_FILE}" ]; then
-  rm -f "${SHARED}/genesis.json"
-fi
-
-export DEPLOY_CONFIG_PATH="${DEPLOY_CONFIG}"
-
-# 3. Generate deploy config file
-if [ ! -f "${DEPLOY_CONFIG_PATH}" ]; then
-  rm -f "${SHARED}/1337-deploy.json"
-
-  echo "Generating deploy config..."
-  /volume/config.sh
-
-  cd "${WORKDIR}"
-
-  # 4. Deploy Optimism L1 contracts
-  DEPLOYMENT_CONTEXT=umi \
-    DEPLOY_CONFIG_PATH="${DEPLOY_CONFIG_PATH}" \
-    IMPL_SALT=0000000000000000000000000000000000000000000000000000000000000000 \
-    forge script scripts/Deploy.s.sol:Deploy \
-    --private-key "${ADMIN_PRIVATE_KEY}" \
-    --broadcast \
-    --rpc-url "${L1_RPC_URL}" \
-    --slow \
-    --legacy \
-    --non-interactive
-fi
-
+echo "${JWT_SECRET}" > "${JWT_FILE}"
 cp -f "${L1_DEPLOYMENT}" "${SHARED}/1337-deploy.json"
-
-# 5. Generate L2 genesis state dump
-if [ ! -f "${L2_ALLOCS}" ]; then
-  cd "${WORKDIR}"
-
-  DEPLOY_CONFIG_PATH="${DEPLOY_CONFIG_PATH}" \
-    CONTRACT_ADDRESSES_PATH="${L1_DEPLOYMENT}" \
-    forge script scripts/L2Genesis.s.sol:L2Genesis \
-    --sig "runWithAllUpgrades()" \
-    --non-interactive
-fi
-
-if [ ! -f "${ROLLUP_FILE}" ]; then
-  # 6. Generate genesis
-  op-node genesis l2 \
-    --deploy-config "${DEPLOY_CONFIG}" \
-    --l1-deployments "${L1_DEPLOYMENT}" \
-    --l2-allocs "${L2_ALLOCS}" \
-    --outfile.l2 "${GENESIS_FILE}" \
-    --outfile.rollup "${ROLLUP_FILE}" \
-    --l1-rpc "${L1_RPC_URL}"
-fi
-
 cp -f "${GENESIS_FILE}" "${SHARED}/genesis.json"
 
-echo "${JWT_SECRET}" >"${JWT_FILE}"
-
-echo "Waiting for op-move DB to initialize and serve the genesis block..."
-while true; do
-  # We ask for block 0. This command will fail with a non-zero exit code
-  # until the op-move database is initialized with the genesis state.
-  # We suppress stdout and stderr to keep the logs clean.
-  if cast block 0 --rpc-url "http://${OP_MOVE_ADDR}:${OP_MOVE_PORT}" >//null 2>&1; then
-    # If the command's exit code is 0 (success), it means op-move
-    # successfully retrieved and returned the genesis block.
-    echo "op-move is fully initialized, proceeding..."
-    break
-  fi
-
-  echo "op-move database is not ready yet, waiting..."
-  sleep 2
-done
+# Wait for op-move to serve the genesis block
+while [ "$(cast block 0 --rpc-url http://${OP_MOVE_ADDR}:${OP_MOVE_PORT} >/dev/null 2>&1 ; echo $?)" -ne 0 ]; do sleep 1; done
 
 op-node \
   --l1.beacon.ignore \
