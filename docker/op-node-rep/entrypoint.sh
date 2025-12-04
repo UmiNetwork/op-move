b0e86c63ca1222d1ff68b68362f8ad5002077b15
Ilia Churin <churin.ilya@gmail.com>
2025-12-04 14:49:01 +0900

    feat(docker): deploy updated op stack

--- docker/op-node-rep/entrypoint.sh
+++ docker/op-node-rep/entrypoint.sh
@@ -6,18 +6,22 @@ WORKDIR="/volume"
 SHARED="${WORKDIR}/shared"
 P2P_DIR="${WORKDIR}/host/p2p"
 ROLLUP_FILE="${SHARED}/rollup.json"
+L1_GENESIS_FILE="${SHARED}/l1_genesis.json"
 JWT_FILE="${WORKDIR}/jwt.txt"
 TIMEOUT_SECS=1500
 PEER_RPC="http://${PEER_ADDR}:${PEER_RPC_PORT}"
 
 # Dump JWT secret token to a file that gets passed as op-node CLI argument
-echo "${JWT_SECRET}" > "${JWT_FILE}"
+echo "${JWT_SECRET}" >"${JWT_FILE}"
 
 # Make dir for persistent P2P data
 mkdir -p ${P2P_DIR}
 
 # Wait for op-move to serve the genesis block
-while [ "$(cast block 0 --rpc-url "${L2_RPC_HTTP_URL}" >/dev/null 2>&1 ; echo $?)" -ne 0 ]; do sleep 1; done
+while [ "$(
+  cast block 0 --rpc-url "${L2_RPC_HTTP_URL}" >/dev/null 2>&1
+  echo $?
+)" -ne 0 ]; do sleep 1; done
 
 # Wait for geth to deploy Optimism
 while [ ! -f "${ROLLUP_FILE}" ]; do sleep 1; done
@@ -27,13 +31,13 @@ wait-for-it -t "${TIMEOUT_SECS}" "${PEER_ADDR}:${PEER_RPC_PORT}"
 
 # Parse peer P2P parameters, wait for PEER_ENR to contain IP (test by length)
 while
-  PEER_P2P=$(curl -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"opp2p_self","params":[],"id":1}' "${PEER_RPC}");
-  PEER_ENR=$(echo "${PEER_P2P}" | sed -e 's/.*\"ENR\"\:\"//g' | sed -e 's/\".*//g');
-  [ ${#PEER_ENR} -lt 220 ];
+  PEER_P2P=$(curl -X POST -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"opp2p_self","params":[],"id":1}' "${PEER_RPC}")
+  PEER_ENR=$(echo "${PEER_P2P}" | sed -e 's/.*\"ENR\"\:\"//g' | sed -e 's/\".*//g')
+  [ ${#PEER_ENR} -lt 220 ]
 do sleep 5; done
 
 # Set peer manually
-PEER_ID=$(echo "${PEER_P2P}" | sed -e 's/.*\"peerID\"\:\"//g' | sed -e 's/\".*//g');
+PEER_ID=$(echo "${PEER_P2P}" | sed -e 's/.*\"peerID\"\:\"//g' | sed -e 's/\".*//g')
 PEER_IP=$(nc -vz "${PEER_ADDR}" "${PEER_P2P_PORT}" 2>&1 | cut -d '(' -f2 | cut -d ':' -f1)
 P2P_STATIC="/ip4/${PEER_IP}/tcp/${PEER_P2P_PORT}/p2p/${PEER_ID}"
 P2P_BOOTNODES="${PEER_ENR}"
@@ -59,7 +63,8 @@ op-node \
   --p2p.static "${P2P_STATIC}" \
   --p2p.priv.path "${P2P_DIR}/priv.txt" \
   --p2p.peerstore.path "${P2P_DIR}/peerstore_db" \
-  --p2p.discovery.path "${P2P_DIR}/discovery_db"  &
+  --p2p.discovery.path "${P2P_DIR}/discovery_db" \
+  --rollup.l1-chain-config "${L1_GENESIS_FILE}" &
 
 # Get the PID of the process launched in the background
 PID="$!"
